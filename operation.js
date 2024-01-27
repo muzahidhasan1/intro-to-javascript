3559fabd06b944698a2cfeeb7a9be8e0ae40b3f9
muzahidhasan1 <muzahiidhasan@gmail.com>
2024-01-28 03:47:06 +0600

    first commit

---
+++ operation.js
@@ -0,0 +1,10 @@
+var eggPrice = 10;
+var onionPrice = 20;
+
+
+// console.log(eggPrice + onionPrice);
+// console.log(eggPrice - onionPrice);
+// console.log(eggPrice * onionPrice);
+// console.log(onionPrice / eggPrice);
+console.log(10 % 3);
+console.log(11 % 7);
---
+++ variable.js
@@ -0,0 +1,25 @@
+var weight = 38;
+var price = 38;
+var year = 1995;
+var money = 2005;
+var age;
+console.log(money);
+console.log(price);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var age = 35;
+age = 17;
+console.log(age)
