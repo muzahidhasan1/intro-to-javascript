07bcf46c55bb2ca9b1e4df9770287f0232000acd
muzahidhasan1 <muzahiidhasan@gmail.com>
2024-01-28 03:57:52 +0600

    added discountprice

--- operation.js
+++ operation.js
@@ -6,5 +6,13 @@ var onionPrice = 20;
 // console.log(eggPrice - onionPrice);
 // console.log(eggPrice * onionPrice);
 // console.log(onionPrice / eggPrice);
-console.log(10 % 3);
-console.log(11 % 7);
+
+var price = 50;
+price = price + 10
+var newPrice = price + 50;
+discount = newPrice - 10;
+// console.log(10 % 3);
+// console.log(11 % 7);
+// console.log(price);
+console.log(newPrice);
+console.log(discount);
